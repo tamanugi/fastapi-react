99eaca2da20fcd735b48279a4ed2a53933504e4b
tamanugi <tamanugi@gmail.com>
2022-05-26 11:56:16 +0900

    api CORS

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core import search_engine
 from app.domain.book import routes as book_router
@@ -6,8 +7,20 @@ from app.domain.book import routes as book_router
 # Opensearch Client setup
 search_engine.set_up()
 
-
 app = FastAPI()
+
+origins = [
+    # TODO: use config
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(book_router.router, prefix="/api")
