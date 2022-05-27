b7aa2ac6971184d6039b156a1916405e4f251c6d
tamanugi <tamanugi@gmail.com>
2022-05-27 17:30:43 +0900

    OpenSearch install japanese plugin

--- migrations/search-engine/migration.sh
+++ migrations/search-engine/migration.sh
@@ -5,15 +5,32 @@ INDEX_NAME=books
 EXEC_CMD="docker compose exec search-engine"
 
 # delete index
-$EXEC_CMD curl -X DELETE "localhost:9200/$INDEX_NAME?pretty"
+$EXEC_CMD curl --user admin:admin -X DELETE --insecure "https://localhost:9200/$INDEX_NAME?pretty"
 
 # create index
-$EXEC_CMD curl -X PUT "localhost:9200/$INDEX_NAME?pretty"
+$EXEC_CMD curl --user admin:admin -X PUT -H "Content-Type: application/json" --insecure  "https://localhost:9200/$INDEX_NAME?pretty" -d'
+{
+    "settings":{
+        "analysis":{
+            "tokenizer" : {
+                "kuromoji" : {
+                   "type" : "kuromoji_tokenizer"
+                }
+            },
+            "analyzer": {
+                "default": {
+                    "type": "custom",
+                    "tokenizer": "kuromoji"
+                }
+            }
+        }
+    }
+}'
 
 # bulk dataset
 cat dataset.json | jq -c '.[] | ({"index":{"_index": "books", "_id": .isbn}}, .)' > tmp_dataset.json
 docker compose cp tmp_dataset.json search-engine:/tmp/tmp_dataset.json 
-$EXEC_CMD curl -X PUT "http://localhost:9200/$INDEX_NAME/_bulk?pretty&refresh"  -H "Content-Type: application/json" --data-binary "@/tmp/tmp_dataset.json"
+$EXEC_CMD curl --user admin:admin -X PUT --insecure "https://localhost:9200/$INDEX_NAME/_bulk?pretty&refresh"  -H "Content-Type: application/json" --data-binary "@/tmp/tmp_dataset.json"
 
 rm tmp_dataset.json
  
