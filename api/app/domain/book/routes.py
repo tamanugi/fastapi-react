66da04a4c3a6e74b22d0960d522eac18affc8eb0
tamanugi <tamanugi@gmail.com>
2022-06-03 15:55:36 +0900

    意図せずインスタンス作っていたのを修正

--- api/app/domain/book/routes.py
+++ api/app/domain/book/routes.py
@@ -14,7 +14,7 @@ router = APIRouter(prefix="/books", tags=["books"])
 @router.get("/search", response_model=BookSearchResponse)
 def search_books(query: BookSearch = Depends()) -> BookSearchResponse:
     response = book_search_service.search(query)
-    books = [BookRead().from_model(book_model) for book_model in response]
+    books = [BookRead.from_model(book_model) for book_model in response]
     return BookSearchResponse(books=books, count=len(books))
 
 
